45416437d65f62ee615d45eaf2027ec888eb4b8c
Dan Applegate <dan@skillshare.com>
2012-07-10 02:35:23 -0400

    Simple list

--- structure/init.php
+++ structure/init.php
@@ -5,3 +5,9 @@ function printRefreshTimestamp() {
 
 $title = "The Doomsday Weekly";
 $tagline = "The Last News Source You'll Ever Need";
+$checklist_items = array(
+    'Dig a bomb shelter',
+    'Stockpile guns for inevitable zombie invasion',
+    'Fashion tinfoil hats to prevent alien mind control',
+    'Obtain SPF 1,000,000 sunscreen for stronger solar rays, due to meteor impact pushing Earth closer to Sun'
+);
