ea4a1138103759a2390dc12aef2e785e5c1712b9
Dan Applegate <dan@skillshare.com>
2012-07-10 04:29:34 -0400

    Database write permissions, db helper functions

---
+++ lib/dbHelpers.php
@@ -0,0 +1,64 @@
+<?php
+$dataPath = realpath(__DIR__ . '/../data/doomsday.sqlite');
+$db = new PDO('sqlite:' . $dataPath);
+
+if (!function_exists('getArticles')) {
+    function getArticles($limit = null) {
+
+        global $db;
+
+        $useLimit = false;
+        $sql = 'SELECT * FROM articles ORDER BY id DESC';
+        if (isset($limit) && is_int($limit)) {
+            $sql .= ' LIMIT ?';
+            $useLimit = true;
+        }
+
+        $statement = $db->prepare($sql);
+
+        if ($useLimit) {
+            $statement->bindParam(1, $limit, PDO::PARAM_INT);
+        }
+
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+if (!function_exists('getArticleBySlug')) {
+    function getArticleBySlug($slug) {
+
+        global $db;
+
+        $statement = $db->prepare('SELECT * FROM articles WHERE slug = ?');
+        $statement->bindParam(1, $slug, PDO::PARAM_STR);
+
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+}
+
+if (!function_exists('getCommentsBySlug')) {
+    function getCommentsBySlug($slug) {
+
+        global $db;
+
+        $statement = $db->prepare('SELECT * FROM comments WHERE article_slug = ?');
+        $statement->bindParam(1, $slug, PDO::PARAM_STR);
+
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+if (!function_exists('insertComment')) {
+    function insertComment($slug, $name, $message) {
+
+        global $db;
+
+        $statement = $db->prepare('INSERT INTO comments (article_slug, name, message) VALUES (?, ?, ?)');
+
+        $statement->execute(array($slug, $name, $message));
+        var_dump($statement->errorInfo());
+    }
+}
