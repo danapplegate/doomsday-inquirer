a3da10fb99bc8c8aad50d9302bdf26e0bc110ec5
Dan Applegate <dan@skillshare.com>
2012-07-10 11:02:08 -0400

    Checkpoint 10

--- news/view.php
+++ news/view.php
@@ -1,5 +1,17 @@
 <?php
 include '../structure/init.php';
+
+// Load database helper functions
+include '../lib/dbHelpers.php';
+
+$slug = 'camping-returns-from-retreat';
+if (isset($_GET['slug'])) {
+    $slug = $_GET['slug'];
+}
+
+// Now retrieve the article from the database, using the slug
+// from the GET parameter
+$article = getArticleBySlug($slug);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -69,17 +81,10 @@ include '../structure/init.php';
         <div class="span12">
           <article>
             <div class="page-header">
-              <h1>Camping Returns from Retreat, Reveals His Latest Prediction</h1>
+                <h1><?php echo $article['title']; ?></h1>
             </div>
-            <img src="../assets/img/harold-camping.jpeg" alt="Harold Camping makes a prediction" class="article pull-left" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum, quam vitae varius volutpat, est massa consectetur dolor, sed euismod nulla felis non nibh. Quisque at risus nec dui sollicitudin fringilla. Etiam lacus elit, adipiscing at gravida eu, commodo id orci. In imperdiet, nunc et laoreet rutrum, quam libero hendrerit odio, et vehicula lectus augue ut turpis. Fusce faucibus quam ut turpis ultrices blandit dapibus felis imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec at convallis nulla. Etiam posuere tempor ipsum, quis dignissim augue venenatis sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus porta rutrum nisl, sit amet rutrum justo dapibus eleifend. Mauris porta, lorem a hendrerit tristique, felis felis porttitor massa, quis egestas quam felis vestibulum risus.</p>
-            <p>Donec pharetra gravida justo, id tincidunt arcu consequat id. Fusce tristique volutpat dui, eu commodo nunc lacinia et. Proin pulvinar tincidunt urna, in placerat felis molestie consectetur. Curabitur eget facilisis urna. Quisque erat sapien, vestibulum quis vulputate in, ultrices ut magna. Duis convallis consequat purus nec tincidunt. Integer urna libero, hendrerit sed aliquet venenatis, mattis in quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis hendrerit fermentum. Mauris augue mauris, euismod non semper in, convallis ac justo. Maecenas aliquam risus quis lorem pulvinar id gravida lectus rhoncus. Sed nisl magna, congue et ornare eu, rhoncus ut urna. Pellentesque diam ligula, bibendum sed hendrerit egestas, semper sodales dui.</p>
-
-            <p>Curabitur tincidunt sollicitudin ornare. Fusce ultricies libero vitae augue aliquam eu sodales velit faucibus. Ut hendrerit dolor nec tellus tincidunt accumsan. Aliquam ipsum magna, accumsan et aliquet non, ultrices in erat. Sed luctus purus vitae purus scelerisque luctus. Sed laoreet fermentum augue sed adipiscing. Sed sed magna quis sapien dignissim gravida.</p>
-
-            <p>Nunc accumsan lacinia tortor quis porta. Phasellus nisi ligula, adipiscing vel porta nec, ultricies eu risus. Duis nec metus orci. Fusce at nisl lacus, ut facilisis tortor. Sed purus dolor, pellentesque ut consectetur ornare, dictum quis sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet erat at magna mollis laoreet vitae a est. Phasellus in justo ut tellus aliquam feugiat. Curabitur condimentum, massa a suscipit imperdiet, enim purus iaculis lacus, sit amet convallis metus lorem et leo. Ut vitae felis vitae metus iaculis eleifend. Nulla mollis, lacus in laoreet mollis, nisl lacus consectetur diam, at aliquet mauris ligula ut nulla.</p>
-
-            <p>Nam posuere dui et quam posuere non consectetur felis adipiscing. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In aliquet elit lectus, congue dictum risus. Aliquam ligula massa, fermentum in tristique in, convallis in magna. Quisque congue laoreet libero, nec feugiat neque pharetra sed. Nam semper posuere pellentesque. Morbi et dignissim arcu. Cras placerat auctor ultrices. Donec sit amet lacus erat, sit amet dictum urna. Mauris tincidunt feugiat felis, vitae mollis magna tempus eu. Fusce tempus euismod mauris ac placerat. Praesent purus ligula, sagittis vitae imperdiet in, condimentum nec neque. Nunc vestibulum, odio sit amet varius bibendum, tortor purus scelerisque leo, at interdum ipsum augue non nibh. Donec ornare consequat ipsum, vel dapibus purus gravida quis.</p>
+            <img src="../assets/img/<?php echo $article['image']; ?>" alt="Article thumbnail" class="article pull-left" />
+            <?php echo $article['content']; ?>
             
             <aside>
               <div class="comment-list span10 offset1">
