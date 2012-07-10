0106c76b21ec66784158d2f2c5e0dce3aa6f681c
Dan Applegate <dan@skillshare.com>
2012-07-10 02:43:29 -0400

    Multi-dimensional arrays

--- structure/init.php
+++ structure/init.php
@@ -6,8 +6,20 @@ function printRefreshTimestamp() {
 $title = "The Doomsday Weekly";
 $tagline = "The Last News Source You'll Ever Need";
 $checklist_items = array(
-    'Dig a bomb shelter',
-    'Stockpile guns for inevitable zombie invasion',
-    'Fashion tinfoil hats to prevent alien mind control',
-    'Obtain SPF 1,000,000 sunscreen for stronger solar rays, due to meteor impact pushing Earth closer to Sun'
+    array(
+        'source' => 'Stephen',
+        'tip' => 'Dig a bomb shelter',
+    ),
+    array(
+        'source' => 'Joe',
+        'tip' => 'Stockpile guns for inevitable zombie invasion',
+    ),
+    array(
+        'source' => 'Abby',
+        'tip' => 'Fashion tinfoil hats to prevent alien mind control',
+    ),
+    array(
+        'source' => 'Mary',
+        'tip' => 'Obtain SPF 1,000,000 sunscreen for stronger solar rays, due to meteor impact pushing Earth closer to Sun',
+    )
 );
